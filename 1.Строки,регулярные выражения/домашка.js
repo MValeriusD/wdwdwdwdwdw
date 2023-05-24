ee90dff43f8a63338ca90ca81e2dbb9191439e9c
MValeriusD <133944948+MValeriusD@users.noreply.github.com>
2023-05-24 10:39:32 +0300

    Исправления домашка

--- 1.Строки,регулярные выражения/домашка.js
+++ 1.Строки,регулярные выражения/домашка.js
@@ -2,7 +2,7 @@
 1. Написать скрипт, предлогающий пользователю ввести две строки через запятую. Вывести сообщение true,
     если вторая строка содержится в первый, в противном случае false, регистр при проверке не учитывать.
  */
-/*
+
 function usersInputStrings(){
    const usersStrings =  prompt("Введите строку через ','");
    const splitStrings = usersStrings.split(',');
@@ -12,18 +12,18 @@ function usersInputStrings(){
 }
 
 console.log(usersInputStrings());
-*/
+
 /*
 2. Пользователь вводит строку, затем число (кол-во символов). Функция усекает строку до кол-ва символов и добавляет многоточие
  */
-/*
+
 function inputString (){
     const inputValue = prompt('Введите строку');
     const inputCountSymbols = prompt('Введите количество символов');
     const stringSlise = inputValue.slice(0,inputCountSymbols);
     return stringSlise + '...';
 }
-*/
+
 // На вход дана строка в CamelCase, преобразовать строку в snake_case
 
 function toUpString (){
